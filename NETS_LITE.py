47c60d9cb54c90d69b049654854926559328d842
Sam Kumagai <s.kumagai3@gmail.com>
2024-12-01 21:00:49 -0500

    got rid of convoluted logic in binary mask scoring, maybe this will work?

--- NETS_LITE.py
+++ NETS_LITE.py
@@ -1330,11 +1330,12 @@ def save_scores_from_fvol(y_true, y_pred, FILE_MODEL, FILE_FIG, score_dict, N_CL
     print(f'Shape of y_pred before processing: {y_pred.shape}')
     print(f'Shape of y_true before processing: {y_true.shape}')
     # adjust single-channel preds to two-channel preds:
-    if y_pred.shape[-1] == 1:
-      y_pred = np.concatenate([1-y_pred, y_pred], axis=-1)
-      print(f'Adjusted y_pred shape: {y_pred.shape}')
+    #if y_pred.shape[-1] == 1:
+      #y_pred = np.concatenate([1-y_pred, y_pred], axis=-1)
+      #print(f'Adjusted y_pred shape: {y_pred.shape}')
     y_true_flat = y_true.flatten()
-    y_pred_flat = y_pred.reshape(-1, N_CLASSES)[:, 1]  # Select positive class probabilities
+    #y_pred_flat = y_pred.reshape(-1, N_CLASSES)[:, 1]  # Select positive class probabilities
+    y_pred_flat = y_pred.flatten()
     # Downsample
     y_true_flat = y_true_flat[::downsample]
     y_pred_flat = y_pred_flat[::downsample]
